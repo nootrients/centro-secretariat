bfe46df827ba1e470805ccc782eb4612d97b711e
oevnaxz <146565062+nootrients@users.noreply.github.com>
2023-10-14 20:57:50 +0800

    Changed Project-Level Permissions

--- centro_secretariat/settings.py
+++ centro_secretariat/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated', # new
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly', 
     ]
 }
 
