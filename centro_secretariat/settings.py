d36d14fffdae66a61d894c8d5ca2b497a9392a1f
my-name-is-gabs <villar.gabriel.p.6@gmail.com>
2023-10-18 11:01:50 +0800

    Super Small Changes

--- centro_secretariat/settings.py
+++ centro_secretariat/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "corsheaders",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -42,6 +41,7 @@ INSTALLED_APPS = [
     # third party
     "rest_framework",
     "rest_framework_simplejwt.token_blacklist",
+    "corsheaders",
     # local
     "index",
     "demographics",
