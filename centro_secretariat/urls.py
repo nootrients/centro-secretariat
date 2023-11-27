a64d745743ecad3552cc38529e0d1a9ffb5acad8
oevnaxz <146565062+nootrients@users.noreply.github.com>
2023-11-27 15:26:47 +0800

    Update settings and urls for deployment

--- centro_secretariat/urls.py
+++ centro_secretariat/urls.py
@@ -28,5 +28,8 @@ urlpatterns = [
     path('survey/', include('survey.urls')),
 ]   
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve media files during development
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
